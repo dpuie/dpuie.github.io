ef83dccba88a61eeb996e6a4c048c58b0f849a32
Denisa Puie <denisapuie86@gmail.com>
2023-11-21 18:52:21 +0000

    extract js in new file

---
+++ examples.js
@@ -0,0 +1,17 @@
+console.info(2 + 3);
+console.warn("denisa");
+
+var employed = true;
+var age = 18;
+console.log("angajat:", employed);
+employed = false;
+console.log("angajat:", employed);
+employed = "da";
+console.log("angajat:", employed);
+
+var skills = ["html", "css"];
+console.debug("tipul variabilei skills", typeof skills);
+
+var job = document.getElementById("job-title");
+console.warm("job", job);
+console.info(typeof job);
