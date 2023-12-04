f90a677b6d184267befb0796e20c3b0ee51ef0f5
Denisa Puie <denisapuie86@gmail.com>
2023-12-04 20:39:09 +0000

    modified index.js, style.css, index.html

--- index.js
+++ index.js
@@ -1,43 +1,20 @@
-function show() {
-  console.info("show", id);
-  var page = document.getElementById(id);
-  console.info("page", page);
-  page.style.display = "block";
-}
+var activePage = "PROJECTS";
 
 function hide(id) {
-  console.info(hide, id);
+  console.info("hide", id);
   document.getElementById(id).style.display = "none";
 }
 
-function showHOME() {
-  hide("SKILLS");
-  hide("PROJECTS");
-  hide("LANGUAGES");
-
-  show("HOME");
+function show(id) {
+  console.info("show", id);
+  var page = document.getElementById(id);
+  console.debug("show page", page);
+  page.style.display = "block";
 }
 
-function showSKILLS() {
-  hide("HOME");
-  hide("PROJECTS");
-  hide("LANGUAGES");
-
-  show("SKILLS");
-}
-
-function showPROJECTS() {
-  hide("HOME");
-  hide("SKILLS");
-  hide("LANGUAGES");
-
-  show("PROJECTS");
-}
-
-function showLANGUAGES() {
-  hide("HOME");
-  hide("SKILLS");
-  hide("PROJECTS");
-
-  show("LANGUAGES");
+function showPage(id) {
+  console.info("show page", id);
+  hide(activePage);
+  show(id);
+  activePage = id;
 }
